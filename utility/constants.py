9d0e9c160f2c0c77d552480c6686232b01da040f
Ryan Maugin <74085084+ryanmaugv1@users.noreply.github.com>
2021-06-20 00:31:37 -0700

    Added missing utility constants file.

---
+++ utility/constants.py
@@ -0,0 +1,21 @@
+"""Helper class for defining global application constant variables.
+
+Authored By Ryan Maugin [TCB] (@ryanmaugv1)
+"""
+
+
+class Constants:
+    """Helper class containing global application constants."""
+    
+    # LOGGER CONSTANTS
+    LOG_SEPARATOR = '======================================================================================'
+
+    # AUTHENTICATION CONSTANTS
+    DEMO_APP_KEY = '[YOUR_DEMO_APP]'
+    DEMO_ID = '[YOUR_DEMO_ACCOUNT_ID]'
+    DEMO_PASS = '[YOUR_DEMO_ACCOUNT_PASSWORD]'
+    IS_DEMO = True
+
+    # IG ACCOUNTS IDs
+    SPREAD_BET_ACCOUNT_ID = '[YOUR_SPREAD_ACCOUNT_ID]'  # e.g. Z3JYYT
+    CFD_ACCOUNT_ID = '[YOUR_CFD_ACCOUNT_ID]' # e.g. Z3JYYU
